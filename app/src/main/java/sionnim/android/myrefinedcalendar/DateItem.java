d24c545634fa1b13784477d952fe4f0f3d72e192
5ionnim <bjm222@naver.com>
2022-04-17 11:00:16 +0900

    Add DateItem.java

---
+++ app/src/main/java/sionnim/android/myrefinedcalendar/DateItem.java
@@ -0,0 +1,42 @@
+package sionnim.android.myrefinedcalendar;
+
+public class DateItem {
+    private Integer date = null;
+    private Integer weekend = null;
+    private boolean isSelected = false;
+    public DateItem(){
+    }
+    public DateItem(Integer date, Integer weekend){
+        this.date = date;
+        this.weekend = weekend;
+    }
+    public DateItem(Integer date, Integer weekend, boolean isSelected){
+        this.date = date;
+        this.weekend = weekend;
+        this.isSelected = isSelected;
+    }
+
+    public DateItem(Integer date){
+        this.date = date;
+    }
+
+    public Integer getDate(){
+        return date;
+    }
+
+    public Integer getWeekend(){
+        return weekend;
+    }
+
+    public boolean getIsSelectd() { return isSelected; }
+
+    public void setDate(Integer date){
+        this.date = date;
+    }
+
+    public void setWeekend(Integer weekend){
+        this.weekend = weekend;
+    }
+
+    public void setIsSelected(boolean isSelected) { this.isSelected = isSelected; }
+}
